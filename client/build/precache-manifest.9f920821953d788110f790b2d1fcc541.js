e90f73bfe56af62e881305c7befb5e4a0bc18179
vigneshv <vigneshv2211@gmail.com>
2020-06-25 18:54:44 +0530

    Ready for deployment to heroku

---
+++ client/build/precache-manifest.9f920821953d788110f790b2d1fcc541.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a288e9c058f20e1cfd26fa5cee6375c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e6169d8acf2473a9e96",
+    "url": "/static/css/main.ef6e2ecb.chunk.css"
+  },
+  {
+    "revision": "81f758e4a513bc2b8b27",
+    "url": "/static/js/2.1f2af332.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f2af332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e6169d8acf2473a9e96",
+    "url": "/static/js/main.be08c360.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
